37e47fcd4401effa1a25ee1c3e72d49feccf4d7a
Svaho <svahoofficial@gmail.com>
2022-10-02 20:57:01 +0200

    chuck

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [state, setState] = useState({
+    fact: ''
+  })
+
+
+  const fetchFact = async () => {
+    const chuckFact = await axios.get(
+      'https://api.chucknorris.io/jokes/random'
+    );
+    console.log(chuckFact.data.value);
+    setState({
+      ...state,
+      fact: chuckFact.data.value
+    });
+  }
+
+  useEffect( () => {
+    fetchFact();
+  }, []
+  )  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-6'>
+            <h1 className='title'>
+              Chuck Norris Facts
+            </h1>
+          </div>
+          <div className='col-6 factCol'>
+            <div className='card'>
+              <div className='card-body'>
+                <div>
+                </div>
+              </div>
+              <div>
+                <button className='btn btn-lg' onClick={fetchFact}>
+                  GET A FACT!
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+      <h2>{state.fact}</h2>
+      </div>
     </div>
   );
 }
